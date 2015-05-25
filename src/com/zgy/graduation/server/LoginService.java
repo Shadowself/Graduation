d2f285a5e0a00768ffebf5697767a2f1448a72bc
Shadowself <zgy1206@163.com>
2015-05-25 12:00:58 +0800

    Revert "GraduationServer"
    This reverts commit 132d4fba30e00eacc58d059658e24aadd1359a1d.

--- src/com/zgy/graduation/server/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.zgy.graduation.server;
-
-import java.util.List;
-
-public interface LoginService {
-
-	public boolean login(List<Object> params);
-}
--- src/com/zgy/graduation/server/pestService.java
+++
@@ -1,9 +0,0 @@
-package com.zgy.graduation.server;
-
-import java.util.List;
-
-public interface pestService {
-
-	public boolean AddPest(List<Object> params);
-	
-}
